af1d0381cd9e1f12f1a2d85581cf41745728355f
Mrinal Mallik <mrinal@nagorik.tech>
2024-05-14 22:30:43 +0600

    newsletter component created

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Analytics from "./components/Analytics";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Newsletter from "./components/Newsletter";
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
       <Analytics />
+      <Newsletter />
     </>
   );
 }
