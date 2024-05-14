84c9c4fc0cfd86ade1a9fabc5d234fc21186153f
Mrinal Mallik <mrinal@nagorik.tech>
2024-05-14 23:05:50 +0600

    newsletter section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,52 +8,62 @@ function Navbar() {
   };
 
   return (
-    <div
-      className="flex bg-black justify-between items-center
-     h-24 p-4 max-w-[1240px] mx-auto text-white"
-    >
-      <h1 className="w-full text-3xl font-bold text-[#00df9a] uppercase">
-        react.
-      </h1>
-      <ul className="hidden md:flex ">
-        <li className="p-4">Home</li>
-        <li className="p-4">Company</li>
-        <li className="p-4">Resources</li>
-        <li className="p-4">About</li>
-        <li className="p-4">Contact</li>
-      </ul>
-      <div>
-        {!nav ? (
-          <AiOutlineMenu size={20} onClick={toggleNav} className="md:hidden" />
-        ) : (
-          <AiOutlineClose size={20} onClick={toggleNav} />
-        )}
-      </div>
-
+    <div className="bg-black">
       <div
-        className={
-          nav
-            ? `fixed top-0 left-0 w-[60%] h-full border-r border-r-gray-900
-              bg-[#000300] easy-in-out duration-300`
-            : "fixed left-[-100%]"
-        }
+        className="flex  justify-between items-center
+     h-24 p-4 max-w-[1240px] mx-auto text-white"
       >
-        <h1 className=" m-4 pt-3 w-full text-3xl font-bold text-[#00df9a] uppercase">
+        <h1 className="w-full text-3xl font-bold text-[#00df9a] uppercase">
           react.
         </h1>
-        <ul className=" uppercase">
-          <li className="p-4 border-b border-gray-600 cursor-pointer">Home</li>
-          <li className="p-4 border-b border-gray-600 cursor-pointer">
-            Company
-          </li>
-          <li className="p-4 border-b border-gray-600 cursor-pointer">
-            Resources
-          </li>
-          <li className="p-4 border-b border-gray-600 cursor-pointer">About</li>
-          <li className="p-4 border-b border-gray-600 cursor-pointer">
-            Contact
-          </li>
+        <ul className="hidden md:flex ">
+          <li className="p-4">Home</li>
+          <li className="p-4">Company</li>
+          <li className="p-4">Resources</li>
+          <li className="p-4">About</li>
+          <li className="p-4">Contact</li>
         </ul>
+        <div>
+          {!nav ? (
+            <AiOutlineMenu
+              size={20}
+              onClick={toggleNav}
+              className="md:hidden"
+            />
+          ) : (
+            <AiOutlineClose size={20} onClick={toggleNav} />
+          )}
+        </div>
+
+        <div
+          className={
+            nav
+              ? `fixed top-0 left-0 w-[60%] h-full border-r border-r-gray-900
+              bg-[#000300] easy-in-out duration-300`
+              : "fixed left-[-100%]"
+          }
+        >
+          <h1 className=" m-4 pt-3 w-full text-3xl font-bold text-[#00df9a] uppercase">
+            react.
+          </h1>
+          <ul className=" uppercase">
+            <li className="p-4 border-b border-gray-600 cursor-pointer">
+              Home
+            </li>
+            <li className="p-4 border-b border-gray-600 cursor-pointer">
+              Company
+            </li>
+            <li className="p-4 border-b border-gray-600 cursor-pointer">
+              Resources
+            </li>
+            <li className="p-4 border-b border-gray-600 cursor-pointer">
+              About
+            </li>
+            <li className="p-4 border-b border-gray-600 cursor-pointer">
+              Contact
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -1,7 +1,41 @@
 import React from "react";
 
 function Newsletter() {
-  return <div></div>;
+  return (
+    <div className="bg-black">
+      <div className=" md:flex justify-between text-white px-4  text-center max-w-[1240px] mx-auto">
+        <div className="py-5">
+          <h1 className="text-md md:text-xl lg:text-2xl font-bold">
+            Want tips & tricks to optimize you flow?
+          </h1>
+          <p className="lg:text-[14px] md:text-[13px] text-[12px]">
+            Galley of type and scrambled it to make .
+          </p>
+        </div>
+        <div className="py-5">
+          <div>
+            <input
+              type="text"
+              placeholder="Enter your email"
+              className="rounded-sm p-1 text-black"
+            />
+            <button
+              className="bg-[#00df9a] text-black px-4 py-1 mt-0 ml-2 
+          rounded-md text-[12px] font-bold"
+            >
+              Notify me
+            </button>
+          </div>
+          <p className="pt-2 text-[12px] md:text-[13px] lg:text-[14px]">
+            We care including versions of Lorem Ipsum.
+          </p>
+          <p className="text-[12px] underline text-[#3c9f7f]">
+            Privacy policy?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Newsletter;
